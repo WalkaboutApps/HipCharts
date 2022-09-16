8c21c8992d432bfaa1c2024d4e34306a40e0bb7a
Fish <walkaboutapps@gmail.com>
2022-09-16 14:10:12 -0700

    Initial Commit

---
+++ FreeCharts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FreeCharts
+//
+//  Created by Fish Sticks on 9/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FreeCharts/FreeChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeChartsApp.swift
+//  FreeCharts
+//
+//  Created by Fish Sticks on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct FreeChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
